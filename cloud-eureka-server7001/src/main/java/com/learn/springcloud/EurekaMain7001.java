2e89946ada07d0d8179cfd04a418a775b4a7ad41
eric_Ao <52972851+aoster97@users.noreply.github.com>
2024-11-26 02:38:01 +0800

    Win1120241126

--- cloud-eureka-server7001/src/main/java/com/learn/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/learn/springcloud/EurekaMain7001.java
@@ -2,14 +2,13 @@ package com.learn.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001
-{
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
-        System.out.println("OK...");
+        SpringApplication.run(EurekaMain7001.class, args);
+        System.out.println("ok...");
     }
 }
---
+++ docu/url.md
@@ -0,0 +1,4 @@
+http://localhost:7001/
+http://localhost:7002/
+http://localhost/consumer/payment/get/2
+http://localhost/consumer/payment/create?serial=121431
