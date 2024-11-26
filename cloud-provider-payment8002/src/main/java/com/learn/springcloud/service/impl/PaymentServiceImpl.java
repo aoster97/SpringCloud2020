2d272425c8f713c9c464b8d6902fef1c32e27405
eric_Ao <52972851+aoster97@users.noreply.github.com>
2024-11-27 03:01:12 +0800

    Win1120241127

---
+++ cloud-provider-payment8002/src/main/java/com/learn/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.learn.springcloud.service.impl;
+
+import com.learn.springcloud.dao.PaymentDao;
+import com.learn.springcloud.entities.Payment;
+import com.learn.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
