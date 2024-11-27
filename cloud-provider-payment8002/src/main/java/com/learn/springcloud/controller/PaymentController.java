f2ddccd03b373c9fba11340ec85da0bdd5b93ef5
aoeroz <1249601502@qq.com>
2024-11-27 18:59:20 +0800

    Mac20241127

--- cloud-provider-payment8002/src/main/java/com/learn/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/learn/springcloud/controller/PaymentController.java
@@ -5,10 +5,12 @@ import com.learn.springcloud.entities.Payment;
 import com.learn.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @auther zzyy
@@ -47,6 +49,22 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录,查询ID: " + id, null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element:" + element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getInstanceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 
 }
--- docu/url.md
+++ docu/url.md
@@ -2,3 +2,6 @@ http://localhost:7001/
 http://localhost:7002/
 http://localhost/consumer/payment/get/2
 http://localhost/consumer/payment/create?serial=121431
+
+#discoveryClient
+http://localhost:8002/payment/discovery
